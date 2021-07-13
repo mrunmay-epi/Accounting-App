01ea300b67f5b793b43094abe99b926b11fb50fa
Mrunmay D <mrunmay.deshpande@extrapreneursindia.com>
2021-07-13 20:30:12 +0530

    commiting changes

--- accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.py
+++ accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.py
@@ -3,8 +3,22 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
+
 class PurchaseInvoice(Document):
-	pass
+    def on_submit(self):
+        exists = frappe.db.exists('Item',
+                                  {'quantity': ('<', self.quantity)})
+        
+        if exists:
+            frappe.throw('No item available')        
+        Payment_entry = frappe.new_doc('Payment Entry')
+        Payment_entry.date = self.date
+        Payment_entry.paid_from = self.credit_to
+        Payment_entry.payment_amount = self.total_amount
+        Payment_entry.payment_transaction_id = self.purchase_invoice_id
+        Payment_entry.save()
+        Payment_entry.submit()
--- accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.py
+++ accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.py
@@ -3,8 +3,25 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+    
+
+        	
+
 class SalesInvoice(Document):
-	pass
+    def on_submit(self):
+        exists = frappe.db.exists('Item',
+                                  {'quantity': ('<', self.quantity)})
+        
+        if exists:
+            frappe.throw('No item available')        
+        Payment_entry = frappe.new_doc('Payment Entry')
+        Payment_entry.date = self.date
+        Payment_entry.paid_from = self.debit_to
+        Payment_entry.payment_amount = self.total_amount
+        Payment_entry.payment_transaction_id = self.sales_invoice_id
+        Payment_entry.save()
+        Payment_entry.submit()
+        			
