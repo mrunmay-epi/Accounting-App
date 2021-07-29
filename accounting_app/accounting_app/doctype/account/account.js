c2e824aa55311dcd1c61de3ca9f1d5aadc95506a
Mrunmay D <mrunmay.deshpande@extrapreneursindia.com>
2021-07-29 12:30:43 +0530

    updations in accounting app

--- accounting_app/accounting_app/doctype/account/account.js
+++ accounting_app/accounting_app/doctype/account/account.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Mrunmay D and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Party Account', {
+frappe.ui.form.on('Account', {
 	// refresh: function(frm) {
 
 	// }
